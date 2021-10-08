36c977af92b2ebb13779b442ef17bf2deceac4c8
vmnguyen <manhnguyen510@gmail.com>
2021-10-08 16:11:56 +0700

    Update proxy setting to ignore ssl certification check

--- fastbrute.go
+++ fastbrute.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bufio"
+	"crypto/tls"
+	"encoding/base64"
 	"flag"
 	"fmt"
+	"net"
 	"os"
+	"strings"
 	"sync"
 
 	"github.com/panjf2000/ants/v2"
@@ -14,9 +18,53 @@ import (
 var (
 	client = &fasthttp.Client{
 		NoDefaultUserAgentHeader: true,
+		//Dial:                     fasthttpproxy.FasthttpHTTPDialer("localhost:8080"),
+		TLSConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 )
 
+func FasthttpHTTPDialer(proxy string) fasthttp.DialFunc {
+	return func(addr string) (net.Conn, error) {
+		var auth string
+
+		if strings.Contains(proxy, "@") {
+			split := strings.Split(proxy, "@")
+			auth = base64.StdEncoding.EncodeToString([]byte(split[0]))
+			proxy = split[1]
+
+		}
+
+		conn, err := fasthttp.Dial(proxy)
+		if err != nil {
+			return nil, err
+		}
+
+		req := "CONNECT " + addr + " HTTP/1.1\r\n"
+		if auth != "" {
+			req += "Proxy-Authorization: Basic " + auth + "\r\n"
+		}
+		req += "\r\n"
+
+		if _, err := conn.Write([]byte(req)); err != nil {
+			return nil, err
+		}
+
+		res := fasthttp.AcquireResponse()
+		defer fasthttp.ReleaseResponse(res)
+
+		res.SkipBody = true
+
+		if err := res.Read(bufio.NewReader(conn)); err != nil {
+			conn.Close()
+			return nil, err
+		}
+		if res.Header.StatusCode() != 200 {
+			conn.Close()
+			return nil, fmt.Errorf("could not connect to proxy")
+		}
+		return conn, nil
+	}
+}
 func doRequest(i interface{}) {
 	url := i.(string)
 	req := fasthttp.AcquireRequest()
@@ -28,9 +76,9 @@ func doRequest(i interface{}) {
 	statusCode := resp.StatusCode()
 	fmt.Printf("[%d] %s \n", statusCode, url)
 }
-func brute(request string, concurrent int, path string) {
+func brute(target string, concurrent int, path string) {
 	defer ants.Release()
-	fmt.Println(request)
+
 	var wg sync.WaitGroup
 	p, _ := ants.NewPoolWithFunc(concurrent, func(i interface{}) {
 		doRequest(i)
@@ -48,7 +96,7 @@ func brute(request string, concurrent int, path string) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		wg.Add(1)
-		url := request + "/" + scanner.Text()
+		url := target + "/" + scanner.Text()
 		_ = p.Invoke(string(url))
 	}
 	wg.Wait()
@@ -70,6 +118,9 @@ func main() {
 
 	var mode int
 	flag.IntVar(&mode, "m", 1, "Mode to scan, 1 for stdin, 2 for request file")
+
+	var proxy string
+	flag.StringVar(&proxy, "x", "http://localhost:8080", "HTTP proxy setting")
 
 	flag.Parse()
 
