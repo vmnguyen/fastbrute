93afc0a9da1f3aed4212b279dbeaee06b3c08e8e
vmnguyen <manhnguyen510@gmail.com>
2021-10-08 16:12:52 +0700

    Update proxy setting to ignore ssl certification check

--- fastbrute.go
+++ fastbrute.go
@@ -3,12 +3,9 @@ package main
 import (
 	"bufio"
 	"crypto/tls"
-	"encoding/base64"
 	"flag"
 	"fmt"
-	"net"
 	"os"
-	"strings"
 	"sync"
 
 	"github.com/panjf2000/ants/v2"
@@ -23,48 +20,6 @@ var (
 	}
 )
 
-func FasthttpHTTPDialer(proxy string) fasthttp.DialFunc {
-	return func(addr string) (net.Conn, error) {
-		var auth string
-
-		if strings.Contains(proxy, "@") {
-			split := strings.Split(proxy, "@")
-			auth = base64.StdEncoding.EncodeToString([]byte(split[0]))
-			proxy = split[1]
-
-		}
-
-		conn, err := fasthttp.Dial(proxy)
-		if err != nil {
-			return nil, err
-		}
-
-		req := "CONNECT " + addr + " HTTP/1.1\r\n"
-		if auth != "" {
-			req += "Proxy-Authorization: Basic " + auth + "\r\n"
-		}
-		req += "\r\n"
-
-		if _, err := conn.Write([]byte(req)); err != nil {
-			return nil, err
-		}
-
-		res := fasthttp.AcquireResponse()
-		defer fasthttp.ReleaseResponse(res)
-
-		res.SkipBody = true
-
-		if err := res.Read(bufio.NewReader(conn)); err != nil {
-			conn.Close()
-			return nil, err
-		}
-		if res.Header.StatusCode() != 200 {
-			conn.Close()
-			return nil, fmt.Errorf("could not connect to proxy")
-		}
-		return conn, nil
-	}
-}
 func doRequest(i interface{}) {
 	url := i.(string)
 	req := fasthttp.AcquireRequest()
